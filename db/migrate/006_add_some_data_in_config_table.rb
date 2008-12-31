5c2d2482476df8eb90b76d6f8e6d4e281598381c
Payal Gupta <cool.payal1985@gmail.com>
2008-12-31 13:07:40 +0530

    migration modified

--- db/migrate/006_add_some_data_in_config_table.rb
+++ db/migrate/006_add_some_data_in_config_table.rb
@@ -1,8 +1,8 @@
 class AddSomeDataInConfigTable < ActiveRecord::Migration
   def self.up
-    Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.big", :value => "160x120")
-    Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.medium", :value => "120x90")
-    Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.small", :value => "60x45")
+    Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.big", :value => "120x160")
+    Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.medium", :value => "90x120")
+    Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.small", :value => "45x60")
     Tog::Config.create(:key => "plugins.tog_conclave.event.image.versions.tiny", :value => "30x30")
   end
 
